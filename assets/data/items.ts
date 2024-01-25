69c08b24e2c622b5441667fb3733f3128cc0a0e9
Herik Vargas <herik02.11@gmail.com>
2024-01-25 00:19:02 -0500

    feat/inventory: basic item list

---
+++ assets/data/items.ts
@@ -0,0 +1,202 @@
+export type ItemEntry = {
+  type: string;
+  name: string;
+}
+
+export const ItemList: ItemEntry[] = [
+  // Misc bones
+  {
+    type: "bone",
+    name: "Monster Bone S",
+  },
+  {
+    type: "bone",
+    name: "Monster Bone M",
+  },
+  {
+    type: "bone",
+    name: "Monster Bone L",
+  },
+  {
+    type: "bone",
+    name: "Monster Bone+",
+  },
+  {
+    type: "bone",
+    name: "Monster Keenbone",
+  },
+  {
+    type: "bone",
+    name: "Monster hardbone",
+  },
+  {
+    type: 'bone',
+    name: 'Quality bone',
+  },
+  {
+    type: 'bone',
+    name: 'Boulder bone',
+  },
+  {
+    type: 'bone',
+    name: 'Ancient bone',
+  },
+  // Misc ore
+  {
+    type: "ore",
+    name: "Malachite Ore",
+  },
+  {
+    type: "ore",
+    name: "Carbalite Ore",
+  },
+  {
+    type: "ore",
+    name: "Fucium ore",
+  },
+  {
+    type: "ore",
+    name: "Dragonite Ore",
+  },
+  {
+    type: "ore",
+    name: "Lightcrystal",
+  },
+  {
+    type: "gem",
+    name: "Novacrystal",
+  },
+  {
+    type: "gem",
+    name: "Dragonvein Crystal",
+  },
+  {
+    type: "mud",
+    name: "Fertile Mud",
+  },
+  {
+    type: "hide",
+    name: "Wingdrake hide",
+  },
+  // Barroth
+  {
+    type: "claw",
+    name: "Barroth Claw",
+  },
+  {
+    type: "shell",
+    name: "Barroth Shell",
+  },
+  {
+    type: "shell",
+    name: "Barroth Carapace",
+  },
+  {
+    type: "shell",
+    name: "Barroth Ridge",
+  },
+  // Jyuratodus
+  {
+    type: "scale",
+    name: "Jyuratodus Fin",
+  },
+  {
+    type: "fang",
+    name: "Jyuratodus Fang",
+  },
+  {
+    type: "scale",
+    name: "Jyuratodus Scale",
+  },
+  {
+    type: "shell",
+    name: "Jyuratodus Shell",
+  },
+  {
+    type: "shell",
+    name: "Jyuratodus Carapace",
+  },
+  {
+    type: "scale",
+    name: "Gajau Scale",
+  },
+  {
+    type: "sac",
+    name: "Aqua Sac",
+  },
+  // Pukei-Pukei
+  {
+    type: "wing",
+    name: "Pukei-Pukei Wing",
+  },
+  {
+    type: "scale",
+    name: "Pukei-Pukei Scale",
+  },
+  {
+    type: "wing",
+    name: "Pukei-Pukei Quill",
+  },
+  {
+    type: "shell",
+    name: "Pukei-Pukei Carapace",
+  },
+  {
+    type: "tail",
+    name: "Pukei-Pukei Tail",
+  },
+  {
+    type: "sac",
+    name: "Poison Sac",
+  },
+  {
+    type: "sac",
+    name: "Toxic Sac",
+  },
+  // Diablos
+  {
+    type: "shell",
+    name: "Diablos Shell",
+  },
+  {
+    type: "shell",
+    name: "Diablos Carapace",
+  },
+  {
+    type: "horn",
+    name: "Twisted Horn",
+  },
+  {
+    type: "horn",
+    name: "Majestic Horn",
+  },
+  {
+    type: "fang",
+    name: "Diablos Fang",
+  },
+  {
+    type: "shell",
+    name: "Diablos Ridge",
+  },
+  {
+    type: "gem",
+    name: "Blos Medula",
+  },
+  // Black Diablos 
+  {
+    type: "shell",
+    name: "Black Diablos Carapace",
+  },
+  {
+    type: "horn",
+    name: "Black Spiral Horn",
+  },
+  {
+    type: "shell",
+    name: "Black Diablos Ridge",
+  },
+  {
+    type: "gem",
+    name: "Wyvern Gem",
+  },
+]
