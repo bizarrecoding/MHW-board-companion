f9346a42c4df40a8c1799f1e23571f3198b90774
Herik Vargas <herik02.11@gmail.com>
2024-09-11 11:46:33 -0500

    fix: .env format not been read by expo

--- service/firebase.ts
+++ service/firebase.ts
@@ -9,12 +9,12 @@ import { getReactNativePersistence } from "./FirebaseRNPersistence";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FB_APP_ID,
 };
 
 // Initialize Firebase
