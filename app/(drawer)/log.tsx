b2110a9938b52452887c986987d87af87de4eabc
Herik Vargas <herik02.11@gmail.com>
2025-01-19 01:04:14 -0500

    fix/ui: added missing add log navigation button

--- app/(drawer)/log.tsx
+++ app/(drawer)/log.tsx
@@ -1,6 +1,9 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { DrawerToggleButton } from "@react-navigation/drawer";
+import { Link } from "expo-router";
 import Drawer from "expo-router/drawer";
-import { useColorScheme } from "react-native";
+import * as React from "react";
+import { Pressable, useColorScheme } from "react-native";
 
 import { HunterLog } from "../../components/screens/drawer/HunterLog";
 import Colors from "../../constants/Colors";
@@ -19,6 +22,20 @@ export default function HunterLogScreen() {
             shadowColor: colorScheme === `dark` ? `transparent` : undefined,
           },
           headerLeft: () => <DrawerToggleButton tintColor={tint} />,
+          headerRight: () => (
+            <Link href="/modal?type=log" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="plus"
+                    size={25}
+                    color={tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
         }}
       />
       <HunterLog />
